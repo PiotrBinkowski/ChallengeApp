c6a4a8735875599895f49fb1f768af250cca4618
Piotr Bińkowski <piotr.binkowski.hds@gmail.com>
2023-11-29 19:04:30 +0100

    Zadanie domowe z czwartego dnia

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var female = true;
+var age = 33;
+
+if (female == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && female == true && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (female == false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (female == false && age >= 18)
+{
+    Console.WriteLine("Pełnoletni mężczyzna");
+}
